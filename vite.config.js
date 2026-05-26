6201f6c8d6549283eee81becb9c9815caff885df
PollaMundial Dev <dev@pollamundial.local>
2026-05-25 19:10:21 -0500

    chore: restrict vitest test discovery to resources/js/tests/

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,6 @@ export default defineConfig({
         environment: 'jsdom',
         globals: true,
         setupFiles: ['./resources/js/tests/setup.js'],
+        include: ['resources/js/tests/**/*.{test,spec}.{js,jsx}'],
     },
 });

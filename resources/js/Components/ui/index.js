2c49352bdeb27a75e6f4f97aea26815725484c01
PollaMundial Dev <dev@pollamundial.local>
2026-05-25 19:24:59 -0500

    chore: add barrel export for ui primitives

---
+++ resources/js/Components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as Chip } from './Chip';
+export { default as Cromo } from './Cromo';
+export { default as Burst } from './Burst';
+export { default as Halftone } from './Halftone';

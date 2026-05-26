c2008e9d2067210e756d58061e3ffd8d90901931
PollaMundial Dev <dev@pollamundial.local>
2026-05-25 19:28:21 -0500

    feat: add halftone utilities and extend Tailwind theme with new colors and font families

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,9 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Inline&family=Bowlby+One&family=Anton&family=Archivo+Black&family=Space+Grotesk:wght@400;500;600;700&family=VT323&family=Press+Start+2P&family=JetBrains+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,33 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                navy:  '#0a1f3d',
+                cream: '#f4ecd8',
+                ink:   '#0e1320',
+                pop: {
+                    red:  '#ff3d3d',
+                    yel:  '#ffd23f',
+                    teal: '#00c2a8',
+                },
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans:    ['Space Grotesk', ...defaultTheme.fontFamily.sans],
+                display: ['Bungee', 'system-ui', 'sans-serif'],
+                body:    ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+                pixel:   ['VT323', 'monospace'],
+                mono:    ['"JetBrains Mono"', 'ui-monospace', 'monospace'],
+            },
+            boxShadow: {
+                'pop-sm':  '2px 2px 0 #0e1320',
+                'pop':     '3px 3px 0 #0e1320',
+                'pop-md':  '4px 4px 0 #0e1320',
+                'pop-lg':  '5px 5px 0 #0e1320',
+                'pop-xl':  '6px 6px 0 #0e1320',
+                'pop-cta': '8px 8px 0 #0e1320',
+            },
+            borderWidth: {
+                '2.5': '2.5px',
             },
         },
     },
